2485e200befb933963280ebaeba231d26a3ff26c
Kristfur <>
2023-05-14 09:46:07 +0000

    Add introduction framework

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def introduction():
+    print('Welcome to Battleships!')
+    print('Here are the rules...')
+
+def run_game():
+    introduction()
+
+run_game()
