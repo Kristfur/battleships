2434e86ddea2f7e9a66bb49d40971c0f226a2378
Kristfur <>
2023-05-14 16:53:33 +0000

    Add board class

--- run.py
+++ run.py
@@ -1,3 +1,43 @@
+
+class Board:
+    """
+    Main board class. Sets board size, type (computer or user), number and
+    types of ships. Has methods for adding ships, making guesses and 
+    printing the board
+    """
+    def __init__(self, size, ships, type):
+        self.size = size
+        self.board = [["\u2610" for x in range(size)] for y in range(size)]
+        self.ships = {}
+        self.type = type
+        self.guesses = []
+        self.ship_coords = []
+
+    # Print board to screen
+    def print(self):
+        for row in self.board:
+            print(row)
+    
+    # Add guess to board, return confirmation if ship was hit
+    def guess(self, x, y):
+        self.guesses.append((x, y))
+        
+        if (x, y) in ship_coords:
+            self.board[x][y] = '\u25A3'
+            return 'Hit'
+        else:
+            self.board[x][y] = '\u25A0'
+            return 'Miss'
+
+    # Add ship to board, if it is player's board, show ships on board
+    def add_ship(self, lenght, x, y, dirX, dirY):
+        for segment in range(lenght):
+            self.ship_coords.append(
+                (x + (dirX[0] * segment), y + (dirY[1] * segment)))
+            if self.type == 'player':
+                self.board[x + (dirX[0] * segment][y + (dirY[1] * segment)] = '\u25A6'
+
+
 def introduction():
     """
     Print introduction and game rules
